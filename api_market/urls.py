6de6346a224feb59fe274f12b74c0352ec5d14c7
believeriver <nobuyuki.galois@gmail.com>
2026-03-21 17:23:56 +0900

    20260321 06 settings urls.

---
+++ api_market/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+app_name = 'market'
+
+router = DefaultRouter()
+# router.register(r'companies', views.CompanyViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+
+    # 追加: 株価取得 API
+    # 例: /stock/7203/ または /stock/7203/?start=2025-01-01
+    # path('stock/<int:ticker>/', views.stock_price, name='stock-price'),
+]
