6018b192b977b45ff98c7fe3989762a1a1425199
believeriver <nobuyuki.galois@gmail.com>
2026-04-26 18:30:25 +0900

    20260426 04 fix: remove Accept-Encoding header to prevent gzip issue

--- api_market/collectors/common/base_scraper.py
+++ api_market/collectors/common/base_scraper.py
@@ -53,7 +53,7 @@ class IFetchDataFromUrl(ABC):
             ),
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Language': 'ja,en-US;q=0.7,en;q=0.3',
-            'Accept-Encoding': 'gzip, deflate, br',
+            #'Accept-Encoding': 'gzip, deflate, br',
             'Connection': 'keep-alive',
         }
 
