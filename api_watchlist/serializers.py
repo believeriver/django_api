60b6997988dfefe0e3a8ca4848a4af8a3f44731f
believeriver <nobuyuki.galois@gmail.com>
2026-04-28 04:41:14 +0900

    20260428 01 update api_watchlist serializer

--- api_watchlist/serializers.py
+++ api_watchlist/serializers.py
@@ -4,12 +4,13 @@ from api_market.models import Company
 
 
 class WatchItemSerializer(serializers.ModelSerializer):
-    company_code   = serializers.CharField(source='company.code', read_only=True)
-    company_name   = serializers.CharField(source='company.name', read_only=True)
-    alert_label    = serializers.CharField(
-                         source='get_alert_status_display', read_only=True
-                     )
-    price_diff_pct = serializers.FloatField(read_only=True)
+    company_code     = serializers.CharField(source='company.code', read_only=True)
+    company_name     = serializers.CharField(source='company.name', read_only=True)
+    company_industry = serializers.SerializerMethodField()
+    alert_label      = serializers.CharField(
+                           source='get_alert_status_display', read_only=True
+                       )
+    price_diff_pct   = serializers.FloatField(read_only=True)
 
     # 登録時に銘柄コードで指定
     company_code_input = serializers.CharField(write_only=True, required=False)
@@ -18,7 +19,7 @@ class WatchItemSerializer(serializers.ModelSerializer):
         model  = WatchItem
         fields = [
             'id',
-            'company_code', 'company_name',
+            'company_code', 'company_name', 'company_industry',
             'company_code_input',
             'target_price', 'current_price',
             'price_diff_pct',
@@ -26,6 +27,12 @@ class WatchItemSerializer(serializers.ModelSerializer):
             'memo',
             'created_at', 'updated_at',
         ]
+
+    def get_company_industry(self, obj):
+        try:
+            return obj.company.information.industry
+        except Exception:
+            return None
         read_only_fields = [
             'id', 'alert_status',
             'created_at', 'updated_at',
